88550a7039baa407f0a01957822d29264a69dbb9
Karnail Singh Choudhary <pradhankarnail@gmail.com>
2022-11-07 18:58:35 +0530

    seo
    Signed-off-by: Karnail Singh Choudhary <pradhankarnail@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,34 +5,19 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from './context/AuthContext';
 import { ChatContextProvider } from './context/ChatContext';
-import { StatusContextProvider } from './context/StatusContext';
-import { hydrate, render } from "react-dom";
+import {  StatusContextProvider } from './context/StatusContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-if (root.hasChildNodes()) {
-  hydrate(<AuthContextProvider>
-    <StatusContextProvider>
-      <ChatContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ChatContextProvider>
-    </StatusContextProvider>
-  </AuthContextProvider>, root);
-} else {
-  render(<AuthContextProvider>
-    <StatusContextProvider>
-      <ChatContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ChatContextProvider>
-    </StatusContextProvider>
-  </AuthContextProvider>, root);
-}
 root.render(
-
+  <AuthContextProvider>
+    <StatusContextProvider>
+      <ChatContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChatContextProvider>
+    </StatusContextProvider>
+  </AuthContextProvider>
 );
 
